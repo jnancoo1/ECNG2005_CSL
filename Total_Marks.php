301ceaf9c2533368a45533b194012d140e8e1019
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2023-03-14 06:50:30 -0400

    Create Total_Marks.php

---
+++ Total_Marks.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $noofcols=35;
+        $var=NULL;
+        echo"<div class='table wrapper'>
+            
+
+<table><tr><th>Subject</th>";
+        for ($u=0;$u<$noofcols;$u++){
+        echo"<th>column x</th>";
+    
+    
+}echo"</tr></table>
+
+        ?>
+    </body>
+</html>";
