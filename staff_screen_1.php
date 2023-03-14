40e60350aafb983b42c238c37b46f2aeb7d13e2f
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2023-03-14 04:05:28 -0400

    Update staff_screen_1.php

--- staff_screen_1.php
+++ staff_screen_1.php
@@ -1,163 +1,183 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
+<html>
+<head>
+	<title>Sample Webpage</title>
+<style>
+  h1 {
+    text-align: center;
+  }
 
- <head>
-        <meta charset="UTF-8">
-        <title>Access Student Info</title>
-    
-     <style>
-     h1 {text-align: center;}
-     body{
-         font-family: "Arial", Sans-Serif;
-         background-color:#F0F8FF;}
-     form{
-    width: 400px;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+  body {
+    font-family: 'Arial', Sans-Serif;
+    background-color: #F0F8FF;
+  }
 
-    text-align: left;
-    margin: auto;
+  table {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  table {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 60%;
+    width: 60%;
+    margin-top: 50px;
+  }
+
+  /* This will center the table on the page */
+
+  header {
+    background-color: #1AA7EC;
+    color: #1AA7EC;
+    padding: 10px;
+  }
+
+  nav ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    position: relative;
+    float: left;
+  }
+
+  nav ul li {
+    display: inline-block;
+    background-color: #1AA7EC;
+  }
+
+  nav a {
+    display: block;
+    padding: 0 10px;
+    color: black;
+    font-size: 18px;
+    line-height: 60px;
+    text-decoration: none;
+  }
+
+  nav ul ul {
+    display: none;
     position: absolute;
-     top: 50%;
-     -ms-transform: translateY(50%);
-     transform: translateY(50%);
-     right: 50%;
-     -ms-transform: translateX(50%);
-     transform: translateX(50%);
-     }
-     input {
-  display: block; /* make the input a block element */
-  width: 100%; /* set the input width to 100% of the parent container */
-  padding: 5px; /* add some padding to the input */
-  box-sizing: border-box; /* include padding and border in the input's width */
+    top: 60px;
+  }
+
+  nav ul li:hover>ul {
+    display: inherit;
+  }
+
+  nav ul ul li {
+    width: 200px;
+    float: none;
+    display: list-item;
+    position: relative;
+    background-color: #333;
+  }
+
+  nav ul ul ul li {
+    position: relative;
+    top: -60px;
+    left: 200px;
+  }
+
+  nav li:hover {
+    background-color: #2c3e50;
+  }
+
+  nav a:hover {
+    color: #fff;
+  }
+
+  table {
+  width: 80vw;
+  height: 40vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-     label {
-  display: block; /* make the label a block element */
-  margin-bottom: 5px; /* add some spacing between the label and input */
+button {
+  width: 20vw;
+  height: 10vh;
+  background-color: #1AA7EC;
+  border-style: solid;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+  border: black;
+  color:black;
+  padding: 2vh 5vw;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 2.5vh;
+  text-align: center;
+
 }
-
-     </style>
-     
-    </head>
-    <h1>Carapachaima East Secondary<br>Staff</h1>
-    <body>      
-        <form method ="post">
-        Enter The ID or The name of the student's whose info you would like to access <br>
-        <br>
-        If You Enter A student's name their ID will be displayed<br>
-        <br>
-        To Access Their Record Then Enter the ID number Provided
-        <br>
-        <br>
-            
-           <Label for "ID"> ID:</label>
-           <input type="number" id="ID" name="ID"><br>
-           <div>
-           </div>
-           <br>
-           <Label for "Password">Student Name:</label>
-           <input type="text" id="Name" name="Name"><br>
-           <button type="submit" button onclick="myFunction()" name="submit">Submit!</button>
-           <a href="">
-  </button>
-</a>
-
-          </form>
-          <?php
-          
-          session_start();
-          
-          
-
-
-            $ids_from_database=[];
-            $names_from_database=[];
-
-
-  
-        if(isset($_POST['submit'])) {
-            $StudentID =$_POST['ID'];            
-            $name=$_POST['Name'];
-             ;
-            //echo $username;
-            //echo$paswd;
-            //$CSVfile=fopen("C:\Users\jonat\OneDrive\Desktop\login_data.csv","r");
-            //while(!feof($CSVfile)){
-              //  $data[]=fgetcsv($CSVfile,100);
-                
-           
-             
-
-
-            //}
-            $flag=false;
-            $flag2=false;
-                     
-
-$user = 'root';
-$password = '';
-  
-$database = 'csl_test_data';
-  
-$servername='localhost:4306';
-$mysqli = new mysqli($servername, $user,
-                $password, $database);
-  
-// Checking for connections
-if (!$mysqli){
-    echo "Connection Unsuccessful!!!";
-}
-$sql="SELECT Student_ID,Name FROM retriver";
-$result = mysqli_query($mysqli,$sql);
-if (mysqli_num_rows($result) > 0) {
-  while($row = mysqli_fetch_assoc($result)) {
-      
-      array_push($ids_from_database,$row["Student_ID"]);
-      array_push($names_from_database,$row["Name"]);
+<?php
+session_start();
+if ($_SESSION['Checker']!=NULL){
     
-      
-      }
-  }
-  $max=count($ids_from_database);
-  
-  for($i=0;$i<$max;$i++){
-  if($name==$names_from_database[$i]){
-      $flag=true;
-      break;
-  }
-  
-      
-  }
-  if ($flag){
-      echo "The Student's ID is ".$ids_from_database[$i];
-       }
-  
-  
-  
-mysqli_close($mysqli);
- 
+echo"
+</style>
 
-        }
-         $max=count($ids_from_database);
+</head>
+<body>
+    <h1> CARAPACHAIMA EAST STAFF</h1>	
+	<main>
+            <form method='post'>
+		<table>
+			<tr>
+				<td><button type='submit'name='button1'>PERSONAL DATA</button></td>
+				<td><button type='submit'button name='button2'>STUDENT MARKS</button></td>
+				<td><button type='submit'button name='button3'>DISCIPLINARY RECORD</button></td>
+			</tr>
+			<tr>
+				<td><button type='submit'button name='button4'>MAKE NEW ENTRY</button></td>
+				<td><button type='submit'button name='button5'>UPDATE RECORD</button></td>
+				<td><button type='subumit'button name='button6'>LOG OUT</button></td>
+			</tr>
+		</table>
+                </form>
+	</main>
+</body>"
+;
 
-        for($i=0;$i<$max;$i++){
-            
-            if ($StudentID!=NULL and $StudentID==$ids_from_database[$i]){
-                
-                header("Location:http://localhost/PhpProject5/index.php");
-                 $_SESSION["ID2Print"]=$ids_from_database[$i];
-                
-                
-            }
-        }
+if (isset($_POST['button1'])){
+    
+    echo"Button 1 Pressed";
+    
+}
+if (isset($_POST['button2'])){
+    
+    echo'Button 2 Pressed';
+    
+}
+if (isset($_POST['button3'])){
+    
+    echo'Button 3 Pressed';
+    
+}
+if (isset($_POST['button4'])){
+    
+    echo'Button 4 Pressed';
+    
+}
+if (isset($_POST['button5'])){
+    
+    echo'Button 5 Pressed';
+    
+}
+if (isset($_POST['button6'])){
+    
+   header('Location:http://localhost/PhpProject2/index.php');
+    
+}
+} 
 
-        
-        ?>
-    </body>
-   </html>
+echo"</html>";
+
